6f74a08789eb083215f2170806e3a89440bea7f1
motty.mio2 <motty.mio2@gmail.com>
2022-07-24 00:06:51 +0900

    merge api calls

--- src/app/line.py
+++ src/app/line.py
@@ -1,27 +1,10 @@
 import os
 from typing import Any
 
-from fastapi import FastAPI
-from pydantic import BaseModel
+# lineapi = os.environ["lineapi"]
 
-lineapi = os.environ["lineapi"]
-app = FastAPI()
 # servers=[{"url": os.environ["url"], "description": "testing"}])
 
 
-@app.get("/health")
-def health_check():
-    return {"status": "OK"}
-
-
-class message(BaseModel):
-    destination: str
-    events: list[dict[str, Any]]
-
-
-@app.post("/webhook")
-def webhook(item: message) -> str:
-    message: str = item.events[0]["message"]
-    print(message)
-
+def message(item: Any) -> str:
     return "ok"
--- src/main.py
+++ src/main.py
@@ -1,7 +1,32 @@
+from typing import Any
+from pydantic import BaseModel
 import uvicorn
 from fastapi import FastAPI
 
+from app import line
+
 app = FastAPI()
 
+
+@app.get("/health")
+def health_check():
+    return {"status": "OK"}
+
+
+class message(BaseModel):
+    destination: str
+    events: list[dict[str, Any]]
+
+
+@app.post("/webhook")
+def webhook(item: message) -> str:
+    message: str = item.events[0]["message"]
+    print(message)
+    ret = line.message(item=message)
+
+    return ret
+
+
 if __name__ == "__main__":
+
     uvicorn.run(app="main:app", host="0.0.0.0", port=5000, reload=True)  # type:ignore
